b503d4fbd1092619db9a90c90e250438b69fd661
Madaras Andrei-Iulian <madarasandreiI@gmail.com>
2023-04-12 19:26:28 +0300

    #Options

---
+++ src/main/java/Design/OptionsTest.java
@@ -0,0 +1,39 @@
+/*
+ * Created by JFormDesigner on Mon Apr 10 04:04:29 EEST 2023
+ */
+
+package Design;
+
+import java.awt.*;
+import GUI.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author ADMIN
+ */
+public class OptionsTest extends Options {
+    public OptionsTest() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Educational license - Madaras Andrei
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "insets 0,hidemode 3",
+            // columns
+            "[grow,fill]",
+            // rows
+            "[grow,fill]"));
+        pack();
+        setLocationRelativeTo(null);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Educational license - Madaras Andrei
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
